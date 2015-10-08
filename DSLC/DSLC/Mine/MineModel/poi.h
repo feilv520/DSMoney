7c9fb9e200ae59c1f4db88691aeaeccd4c90b4b5
feilv520 <416751309@qq.com>
2015-10-08 14:53:23 +0800

    000

---
+++ DSLC/DSLC/Mine/MineModel/poi.h
@@ -0,0 +1,13 @@
+//
+//  poi.h
+//  DSLC
+//
+//  Created by ios on 15/10/8.
+//  Copyright © 2015年 马成铭. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface poi : NSObject
+
+@end
---
+++ DSLC/DSLC/Mine/MineView/iii.m
@@ -0,0 +1,21 @@
+//
+//  iii.m
+//  DSLC
+//
+//  Created by ios on 15/10/8.
+//  Copyright © 2015年 马成铭. All rights reserved.
+//
+
+#import "iii.h"
+
+@implementation iii
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
