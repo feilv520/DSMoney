2a334748615084f9d0018b2ac7da750c28dc1105
马成铭 <machengming@machengmingdeiMac.local>
2015-10-08 10:17:44 +0800

    DSLC

---
+++ DSLC/DSLC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSLC
+//
+//  Created by 马成铭 on 15/10/8.
+//  Copyright © 2015年 马成铭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DSLC/DSLCTests/DSLCTests.m
@@ -0,0 +1,39 @@
+//
+//  DSLCTests.m
+//  DSLCTests
+//
+//  Created by 马成铭 on 15/10/8.
+//  Copyright © 2015年 马成铭. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DSLCTests : XCTestCase
+
+@end
+
+@implementation DSLCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
