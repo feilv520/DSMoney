44acc6358eca0d778938891d0c48f20c35cd521b
feilv520 <416751309@qq.com>
2016-03-17 18:03:44 +0800

    1

---
+++ DSLC/DSLC/Financing/FinancingModel/TRedBagModel.m
@@ -0,0 +1,17 @@
+//
+//  TRedBagModel.m
+//  DSLC
+//
+//  Created by ios on 16/3/17.
+//  Copyright © 2016年 马成铭. All rights reserved.
+//
+
+#import "TRedBagModel.h"
+
+@implementation TRedBagModel
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key{
+    
+}
+
+@end
